ef63c6e7fbba5933f2a683a593fd39726b1fa500
Tran Quang Minh <moonlig73@gmail.com>
2024-06-29 22:37:11 +0700

    split_pdf_into_chunk

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import os
 import streamlit as st
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+
 def main():
   load_dotenv()
   st.set_page_config(page_title="Ask your PDF")
@@ -17,7 +19,20 @@ def main():
     for page in pdf_reader.pages:
       text += page.extract_text()
 
-    st.write(text)
+    # split into chunks
+
+    # cấu hình
+    text_splitter = CharacterTextSplitter(
+        #   tách lấy từng câu
+        separator="\n",
+        # 1 lần lấy 1000 ký tự
+        chunk_size=1000,
+        # thụt lui sau 200 từ rồi lấy
+        chunk_overlap=200,
+        length_function=len,
+    )
+    chunks = text_splitter.split_text(text)
+    st.write(chunks)
 
 
 if __name__ == "__main__":
